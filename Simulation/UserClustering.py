cb0253ee367eece2cbce7e974f7fc286077ebbcf
nana <-add>
2018-05-31 13:45:48 +0200

    Fixed clustering

--- Simulation/UserClustering.py
+++ Simulation/UserClustering.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.cluster import MiniBatchKMeans
 
 user_dimension = 100
-clusters = 10
+
 users = np.array([])
 user_count = 0
 
@@ -17,7 +17,7 @@ for line in input:
 
 input.close()
 print(user_count)
-users = users.reshape([user_count, user_dimension])
+users = users.reshape([user_dimension, user_count])
 users = users.transpose()
 
 print("Done with reading data.", flush=True)
@@ -26,12 +26,12 @@ print("Starting clustering.", flush=True)
 batch_size = 1000
 # find the best cluster number
 best_score = float('inf')
-best_nclusters = 0
+best_nclusters = 5
 for nclusters in range(4, 11):
 	mbk = MiniBatchKMeans(init='k-means++', n_clusters=nclusters, batch_size=batch_size,
                       n_init=10, max_no_improvement=10, verbose=0)
 	mbk.fit(users)
-	score = mbk.score(users)
+	score = -mbk.score(users)
 	if score < best_score:
 		best_score = score
 		best_nclusters = nclusters
@@ -47,11 +47,12 @@ output.write(header)
 mbk = MiniBatchKMeans(init='k-means++', n_clusters=best_nclusters, batch_size=batch_size,
                       n_init=10, max_no_improvement=10, verbose=0)
 transformed_users = mbk.fit_transform(users)
-
+print(transformed_users.shape)
 index = 0
 for line in input:
 	line_split = line.split(",")
-	user_str = np.array2string(transformed_users[index], precision=3, separator=' ', suppress_small=True)[1:-1].replace('\n', '')
+	norm = transformed_users[index]/ np.linalg.norm(transformed_users[index])
+	user_str = np.array2string(norm, precision=3, separator=' ', suppress_small=True)[1:-1].replace('\n', '')
 	output.write('{0},{1},{2}\n'.format(user_str, line_split[1], line_split[2]))
 	index += 1
 
