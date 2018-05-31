8dc883a6983b65a001be874d113dfee8cfac80dc
nana <-add>
2018-05-31 15:13:38 +0200

    Updated to cluster on 100000 and then calculate distance to them

--- Simulation/UserClustering.py
+++ Simulation/UserClustering.py
@@ -1,37 +1,40 @@
-import numpy as np
-from sklearn.cluster import MiniBatchKMeans
-
+import numpy as np 
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
 user_dimension = 100
-
+leaning_size = 100000
 users = np.array([])
-user_count = 0
+
+def get_e_distance(x, y):
+	return np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
 
 input = open('./Data.csv', "r")
 input.readline() # skip header
 
+user_count = 0
 for line in input:
 	line_split = line.split(",")
 	user = np.fromstring(line_split[0], dtype=float, sep=' ')
 	users = np.append(users, user)
-	user_count += 1 
-
+	user_count += 1
+	if user_count >= leaning_size:
+		break
 input.close()
-print(user_count)
+
 users = users.reshape([user_dimension, user_count])
 users = users.transpose()
 
 print("Done with reading data.", flush=True)
 print("Starting clustering.", flush=True)
 
-batch_size = 1000
 # find the best cluster number
 best_score = float('inf')
 best_nclusters = 5
+
 for nclusters in range(4, 11):
-	mbk = MiniBatchKMeans(init='k-means++', n_clusters=nclusters, batch_size=batch_size,
-                      n_init=10, max_no_improvement=10, verbose=0)
+	mbk = KMeans(init='k-means++', n_clusters=nclusters, n_init=10)
 	mbk.fit(users)
-	score = -mbk.score(users)
+	score = - mbk.score(users)
 	if score < best_score:
 		best_score = score
 		best_nclusters = nclusters
@@ -39,22 +42,28 @@ for nclusters in range(4, 11):
 
 
 print ("Done learning clusters. Starting updating data.")
+
 output = open('./ClusteredData.csv', "w")
 input = open('./Data.csv', "r")
 header = input.readline() # skip header
 output.write(header)
 
-mbk = MiniBatchKMeans(init='k-means++', n_clusters=best_nclusters, batch_size=batch_size,
-                      n_init=10, max_no_improvement=10, verbose=0)
-transformed_users = mbk.fit_transform(users)
-print(transformed_users.shape)
+mbk = KMeans(init='k-means++', n_clusters=best_nclusters, n_init=10)
+mbk.fit(users)
+
+cluster_centers = mbk.cluster_centers_
+
 index = 0
 for line in input:
 	line_split = line.split(",")
-	norm = transformed_users[index]/ np.linalg.norm(transformed_users[index])
-	user_str = np.array2string(norm, precision=3, separator=' ', suppress_small=True)[1:-1].replace('\n', '')
+	user = np.fromstring(line_split[0], sep=" ")
+	user_to_cluster = np.apply_along_axis(lambda c: get_e_distance(user, c), 1,cluster_centers)
+	user_norm = user_to_cluster / np.linalg.norm(user_to_cluster)
+	user_str = np.array2string(user_norm, precision=3, separator=' ', suppress_small=True)[1:-1].replace('\n', '')
 	output.write('{0},{1},{2}\n'.format(user_str, line_split[1], line_split[2]))
 	index += 1
+	if index % 10000 == 0:
+		print('.', end='', flush=True)
 
 input.close()
 output.close()
