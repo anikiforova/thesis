feb73e6a26ceb81a698b1e700a5be57b0e1ec5cc
nana <-add>
2018-05-31 11:56:39 +0200

    Moved to using MiniBatchKMeans from sklearn

--- Simulation/UserClustering.py
+++ Simulation/UserClustering.py
@@ -1,6 +1,5 @@
 import numpy as np
-import skfuzzy as fuzz
-# from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 
 user_dimension = 100
 clusters = 10
@@ -24,36 +23,43 @@ users = users.transpose()
 print("Done with reading data.", flush=True)
 print("Starting clustering.", flush=True)
 
+batch_size = 1000
 # find the best cluster number
-for ncenters in range(5, 10):
-	cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(users, ncenters, 2, error=0.005, maxiter=1000, init=None)
-	print('Centers = {0}; FPC = {1:.3f}'.format(ncenters, fpc), flush=True)
+best_score = float('inf')
+best_nclusters = 0
+for nclusters in range(4, 11):
+	mbk = MiniBatchKMeans(init='k-means++', n_clusters=nclusters, batch_size=batch_size,
+                      n_init=10, max_no_improvement=10, verbose=0)
+	mbk.fit(users)
+	score = mbk.score(users)
+	if score < best_score:
+		best_score = score
+		best_nclusters = nclusters
+	print('Centers = {0}; FPC = {1:.3f}'.format(nclusters, score), flush=True)
 
-best_cluster_size = 5
-cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(users, best_cluster_size, 2, error=0.005, maxiter=1000, init=None)
 
+# print ("Done learning clusters. Starting updating data.")
+# output = open('./ClusteredData.csv', "w")
+# input = open('./Data.csv', "r")
+# header = input.readline() # skip header
+# output.write(header)
 
-print ("Done learning clusters. Starting updating data.")
-output = open('./ClusteredData.csv', "w")
-input = open('./Data.csv', "r")
-header = input.readline() # skip header
-output.write(header)
-
-u, u0, d, jm, p, fpc = fuzz.cluster.cmeans_predict(users, cntr, 2, error=0.005, maxiter=1000)
-print(u.shape)
-
-index = 0
-for line in input:
-	line_split = line.split(",")
+mbk = MiniBatchKMeans(init='k-means++', n_clusters=nclusters, batch_size=batch_size,
+                      n_init=10, max_no_improvement=10, verbose=0)
+transformed_users = mbk.fit_transform(users)
+print(transformed_users[0])
+# index = 0
+# for line in input:
+# 	line_split = line.split(",")
 	
-	user = np.array([])
-	for i in range(0, best_cluster_size):
-		user = np.append(user, u[i][index])
+# 	user = np.array([])
+# 	for i in range(0, best_cluster_size):
+# 		user = np.append(user, u[i][index])
 	
-	user_str = np.array2string(user, precision=3, separator=' ', suppress_small=True)[1:-1].replace('\n', '')
-	output.write('{0},{1},{2}\n'.format(user_str, line_split[1], line_split[2]))
-	index += 1
+# 	user_str = np.array2string(user, precision=3, separator=' ', suppress_small=True)[1:-1].replace('\n', '')
+# 	output.write('{0},{1},{2}\n'.format(user_str, line_split[1], line_split[2]))
+# 	index += 1
 	
 
-input.close()
-output.close()
+# input.close()
+# output.close()
