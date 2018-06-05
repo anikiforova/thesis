6c3f59254898772a8f08285624834b3f12fc95b2
nana <-add>
2018-06-05 10:12:15 +0200

    Ready for LinUCB_Disjoint

--- Simulation/Run_Algos.py
+++ Simulation/Run_Algos.py
@@ -22,12 +22,12 @@ total_lines = 635387.0 # per file - 12 files
 alphas = {
 		AlgorithmType.Random: 			np.arange(0.05, 0.1, 0.05), # no point in different alphas
 		# AlgorithmType.EFirst:			np.arange(0.05, 0.1, 0.05),
-		AlgorithmType.EGreedy:			np.arange(0.001, 0.3, 0.05),
+		AlgorithmType.EGreedy:			np.arange(0.1, 0.3, 0.05),
 		AlgorithmType.LinUCB_Disjoint:	np.arange(0.05, 0.3, 0.05), # starts decreasing around at 0.25 
 		# AlgorithmType.LinUCB_GP:		np.arange(0.05, 0.1, 0.05),
 		# AlgorithmType.LinUCB_GP_All:	np.arange(0.05, 0.1, 0.05),
 		# AlgorithmType.LinUCB_Hybrid:	np.arange(0.05, 0.1, 0.05),
-		AlgorithmType.UCB:				np.arange(0.001, 0.3, 0.05), # limit to only 1 since same value for different alphas
+		AlgorithmType.UCB:				np.arange(0.05, 0.3, 0.05), # limit to only 1 since same value for different alphas
 		# AlgorithmType.EGreedy_Seg:		np.arange(0.05, 0.1, 0.05),
 		AlgorithmType.EGreedy_Disjoint:	np.arange(0.05, 0.3, 0.05), 
 		# AlgorithmType.EGreedy_Hybrid:	np.arange(0.05, 0.1, 0.05), 
